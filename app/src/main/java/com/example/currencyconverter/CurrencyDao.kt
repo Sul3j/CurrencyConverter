c72ab7dfb94bb483e2c1c58a3b3c097667fff7f6
Szymon Sulejczak <contact@szymonsulejczak.com>
2023-10-21 09:35:04 +0200

    add CurrencyDao

---
+++ app/src/main/java/com/example/currencyconverter/CurrencyDao.kt
@@ -0,0 +1,16 @@
+package com.example.currencyconverter
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface CurrencyDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertCurrency(currency: CurrencyEntity)
+
+    @Query("SELECT * FROM currencies")
+    fun getAllCurrencies(): LiveData<List<CurrencyEntity>>
+}
